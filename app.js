1573caed2218466cedfb7dd5214ba3a764bc502b
rimaulana <rimaulana@outlook.com>
2018-11-12 21:37:36 -0500

    add server IP address

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const ifs = require('os').networkInterfaces();
+
+const getIP = () => {
+  const result = Object.keys(ifs)
+    .map(x => ifs[x].filter(y => y.family === 'IPv4' && !x.internal)[0])
+    .filter(x => x)[0].address;
+  return result;
+};
 
 app.use(morgan('combined'));
 
 
 app.get('/', (req, res) => {
-  res.send('<html><h1 style="text-align:center;">It works!</h1></html>');
+  res.send(`<html><div style="text-align:center;"><h1>It works!</h1><p>server address ${getIP()}</p></div></html>`);
 });
 
 app.get('/health', (req, res) => {
