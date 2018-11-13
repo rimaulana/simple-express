149d75d36760952b84d3e4913a6453dde9ee0792
rimaulana <rimaulana@outlook.com>
2018-11-12 21:58:05 -0500

    metadata to get ip

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
+const metadataURL = 'http://169.254.170.2/v2/metadata/';
 const port = process.env.PORT || 3000;
-
-const ifs = require('os').networkInterfaces();
-
-const getIP = () => {
-  const result = Object.keys(ifs)
-    .map(x => ifs[x].filter(y => y.family === 'IPv4' && !x.internal)[0])
-    .filter(x => x)[0].address;
-  return result;
-};
 
 app.use(morgan('combined'));
 
-
 app.get('/', (req, res) => {
-  res.send(`<html><div style="text-align:center;"><h1>It works!</h1><p>server address ${getIP()}</p></div></html>`);
+  axios.get(metadataURL)
+    .then((result) => {
+      res.send(`<html><div style="text-align:center;"><h1>It works!</h1><p>server address ${result.data.Containers[0].Networks[0].IPv4Addresses[0]}</p></div></html>`);
+    })
+    .catch((error) => {
+      res.status(200).send(error.message);
+    });
 });
 
 app.get('/health', (req, res) => {
